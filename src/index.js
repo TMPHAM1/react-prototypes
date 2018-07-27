6e21100b77804f6dcd805c83583fa713344757fa
Tien Pham <tmpham1@uci.edu>
2018-07-27 00:08:35 -0700

    react-intro completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () =>  { 
+    return Math.round((Math.random()*1000 ) + 1);
+}
+
+const userObj = {
+    name: "Tien",
+    luckyNumber: luckyNumber(),
+};
+const greeting = (userObj) => {
+    return <div className="container"> <h1>Hello {userObj.name} </h1> ><h2 className="text-muted">Your Lucky Number is: {userObj.luckyNumber}</h2></div>
+}
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
+
+
+
+
+
+
